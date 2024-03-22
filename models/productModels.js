0be671113aa52da933aa035d5b3ce31edf65b0fc
Nishant nayak <nayaknishant656@gmail.com>
2024-03-22 22:32:55 +0530

    first commit

---
+++ models/productModels.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const productSchema = mongoose.Schema(
+    {
+        si: {type: String, required: true},
+        name: {type: String, required: true},
+        amount: {type: Number, required: true},
+        adress: {type: String, required: true},
+        year: {type: Number, required: true},
+        img: {type: String, required: true},
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Product = mongoose.model('shoes', productSchema);
+
+export default Product;
